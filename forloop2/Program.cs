fd194175b56dad4ce9b564836fc563ed16be9bb4
nikhildesai6666 <desainikhil9664@gmail.com>
2022-09-22 09:17:24 +0530

    Add project files.

---
+++ forloop2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forloop2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 2; i <= num/2; i++)
+            {
+                if(num%2 == 0)
+                {
+                    Console.WriteLine($"number is not primr , {num}");
+                }
+                else
+                {
+                    Console.WriteLine($" number is primr , {num}");
+                }
+            }
+          
+        }
+    }
+}
---
+++ forloop2/fibnosis.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forloop2
+{
+    internal class fibnosis
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0, n2 = 1;
+            Console.WriteLine("Enter the number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 2; i < num; i++) ;
+            {
+                int n3 = n1 + n2;
+                Console.WriteLine(n3);
+            }
+            
+                
+        }
+}
